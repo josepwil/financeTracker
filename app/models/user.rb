2ceea8e2f3c02f31ed6b9b254f715b738e6cf206
Joseph Williams <josephwilliamsjs@gmail.com>
2021-06-06 10:26:25 -0700

    implement search friends method

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,27 @@ class User < ApplicationRecord
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymous"
   end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    to_send_back ? to_send_back : nil
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
   
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
   
 end
